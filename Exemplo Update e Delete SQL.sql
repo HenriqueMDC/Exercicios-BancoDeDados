eb479fae799837ea4955db227012f3cb3ff7b59a
Henque Mateus Dalfovo Campos <hmdcampos@gmail.com>
2018-06-26 16:08:07 -0300

    Exemplos, exercicio finalizado e lista atualizada

---
+++ Exemplo Update e Delete SQL.sql
@@ -0,0 +1,11 @@
+﻿-- UPDATE outfit SET tamanho = 'XGG' WHERE marca = 'Lelis blanc';
+
+-- UPDATE outfit SET cor = 'Verde', valor = 1.50 WHERE cor = 'Verde';
+
+-- UPDATE outfit SET marca = 'Oakley' WHERE marca = 'Okle';
+
+-- UPDATE outfit SET cor = 'Azul espacial' WHERE tecifo LIKE '%malha%';
+
+-- DELETE FROM outfit WHERE cor = 'Azul espacial';
+
+SELECT * FROM outfit;
--- Exercicios SELECT 3 SQL.sql
+++ Exercicios SELECT 3 SQL.sql
@@ -38,4 +38,40 @@
 -- SELECT TOP 1 nome, nota_1, nota_2, nota_3, nota_4, ((nota_1 + nota_2 + nota_3 + nota_4) / 4) AS 'Média' FROM alunos ORDER BY 'Média' DESC;
 
 -- 14° Selecione o nome, média e caso a média for menor que 5 apresentar reprovado, caso for menor que 7 apresentar em exame senão apresentar aprovado. Dica: buscar como fazer IF em sql.
-SELECT nome, nota_1, nota_2, nota_3, nota_4, ((nota_1 + nota_2 + nota_3 + nota_4) / 4) AS 'Média', IIF(((nota_1 + nota_2 + nota_3 + nota_4) / 4) < 5, 'Reprovado', 'Aprovado') FROM alunos;
+-- SELECT nome, nota_1, nota_2, nota_3, nota_4, ((nota_1 + nota_2 + nota_3 + nota_4) / 4) AS 'Média', IIF(((nota_1 + nota_2 + nota_3 + nota_4) / 4) < 5, 'Reprovado', IIF(((nota_1 + nota_2 + nota_3 + nota_4) / 4) < 7, 'Exame', 'Aprovado')) AS 'Situação' FROM alunos;
+
+-- 15° Selecione o nome, nota 1, nota 2, nota 3, nota 4 com o menor média.
+-- SELECT TOP 1 nome, nota_1, nota_2, nota_3, nota_4, ((nota_1 + nota_2 + nota_3 + nota_4) / 4) AS 'Média' FROM alunos ORDER BY 'Média' ASC;
+
+-- 16° Selecione a quantidade de alunos em que a média foi maior que 7.
+-- SELECT COUNT(nota_1 + nota_2 + nota_3 + nota_4) / 4 AS 'Quantidade de Alunos com média maior que 7' FROM alunos WHERE (nota_1 + nota_2 + nota_3 + nota_4) / 4 > 7;
+
+-- 17° Selecione o nome, nick do aluno que o nick contém 5 caracteres.
+-- SELECT nome, nick FROM alunos WHERE LEN(nick) = 5;
+
+-- 18° Selecione o nome do aluno quando a cor for ouro ou amarelo-torrado e a média for maior que seis e meio.
+-- SELECT nome FROM alunos WHERE (cor_preferida = 'ouro' OR cor_preferida = 'amarelo-torrado') AND (nota_1 + nota_2 + nota_3 + nota_4) / 4 > 6.5;
+
+-- 19° Selecione o nome e o ano da data de nascimento.
+-- SELECT nome, YEAR(data_nascimento) AS 'Ano' FROM alunos;
+
+-- 20° Selecione o nick e o mês de nascimento quando o mês de nascimento for maior que 6.
+-- SELECT nick, MONTH(data_nascimento) AS 'Mês' FROM alunos WHERE MONTH(data_nascimento) = 6;
+
+-- 21° Selecione a quantidade de pessoas que nasceram no ano de 1996.
+-- SELECT COUNT(data_nascimento) FROM alunos WHERE YEAR(data_nascimento) =1996;
+
+-- 22° Selecione a quantidade de pessoas de pessoas que nasceram no dia dois do mês de fevereiro do ano 1964 ou 1994.
+-- SELECT COUNT(YEAR(data_nascimento)) FROM alunos WHERE DAY(data_nascimento) = 2 AND MONTH(data_nascimento) = 2 AND (YEAR(data_nascimento) = 1964 OR YEAR(data_nascimento) = 1994);
+
+-- 23° Selecione o nick e a nota 4 do aluno que a nota 2 está entre 5.0 e 5.99.
+-- SELECT nick, nota_2, nota_4 FROM alunos WHERE nota_2 > 5.0 AND nota_2 <= 5.99;
+
+-- 24° Apresentar a média da aluna Josefina.
+-- SELECT nome, nota_1, nota_2, nota_3, nota_4, (nota_1 + nota_2 + nota_3 + nota_4) / 4 FROM alunos WHERE nome LIKE 'Josefina%';
+
+-- 25° Apresentar nome, nick, nota 1, nota 2, nota 3, nota 4, quando o nome conter Justino e o signo começar com ‘A’.
+-- SELECT nome, nick, nota_1, nota_2, nota_3, nota_4, signo FROM alunos WHERE nome LIKE '%Justino%' AND signo LIKE 'A%';
+
+-- 26° Apresentar a média das médias.
+-- SELECT AVG(nota_1 + nota_2 + nota_3 + nota_4) AS 'Média das média' FROM alunos;
